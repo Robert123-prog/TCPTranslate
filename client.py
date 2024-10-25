fb3ad911c35b6bc7cd93b37119c7d5ae46e6d2a6
OOP <roeminovici@gmail.com>
2024-10-25 16:33:08 +0300

    server uses fork + exception handlings

--- client.py
+++ client.py
@@ -4,6 +4,11 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # s.connect(('193.231.20.76', 5556))
 s.connect(('127.0.0.1', 6969))
 
-s.send(b"Salut frate!")
-print(s.recv(1000))
-s.close()
+try:
+    s.send(b"Salut frate!")
+    response = s.recv(1000).decode('utf-8')
+    print(response)
+except Exception as e:
+    print('Connection error')
+finally:
+    s.close()
--- server.py
+++ server.py
@@ -4,55 +4,60 @@ from googletrans import Translator
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 6969))
+s.listen()
 
 languages = {}
 with open('languages', 'r', encoding='utf-8') as f:
     for line in f:
         # dai split dupa space => lista
         parts = line.split()
-
         language_name = ' '.join(parts[:-1])
         abbreviation = parts[-1] #ultimu part din rand = abrevierea
-
         languages[language_name] = abbreviation
 
 translator = Translator()
 
-s.listen()
-cs, addr = s.accept()
+while True:
+    cs, addr = s.accept()
 
-phrase = cs.recv(1000).decode('utf-8') #trebe decodat
+    if os.fork() == 0:
+        # inchizi socketu "parent"
+        s.close()
 
+        try:
+            # trebe decodat
+            phrase = cs.recv(1000).decode('utf-8')
 
-print('Please use one of the following abbreviations: ')
-print(languages)
-language_to_translate_to = input('Enter the language code to translate to: ')
+            print('Please use one of the following abbreviations: ')
+            print(languages)
+            language_to_translate_to = input('Enter the language code to translate to: ')
 
+            detected = translator.detect(phrase)
+            translation = translator.translate(phrase, src=detected.lang, dest=language_to_translate_to)
 
-detected = translator.detect(phrase)
-translation = translator.translate(phrase, src=detected.lang, dest=language_to_translate_to)
+            cs.send(translation.text.encode('utf-8'))  # encodezi la loc
+        except Exception as e:
+            print('Error during translation')
+        finally:
+            cs.close()
+            os._exit(0)
+    else:
+        cs.close()
 
-cs.send(translation.text.encode('utf-8'))  # encodezi la loc
-
-cs.close()
-
-# while True:
-#     s.listen()
-#     cs, addr = s.accept()
+#s.listen()
+# cs, addr = s.accept()
 #
-#     if os.fork() == 0:
-#         s.close() #inchizi socketu "parent"
+# phrase = cs.recv(1000).decode('utf-8') #trebe decodat
 #
-#         phrase = cs.recv(1000).decode('utf-8') #trebe decodat
 #
-#         language_to_translate_to = input('Enter the language code to translate to: ')
+# print('Please use one of the following abbreviations: ')
+# print(languages)
+# language_to_translate_to = input('Enter the language code to translate to: ')
 #
-#         detected = translator.detect(phrase)
-#         translation = translator.translate(phrase, src=detected.lang, dest=language_to_translate_to)
 #
-#         cs.send(translation.text.encode('utf-8'))  # encodezi la loc
+# detected = translator.detect(phrase)
+# translation = translator.translate(phrase, src=detected.lang, dest=language_to_translate_to)
 #
-#         cs.close()
-#     else:
-#         cs.close()
-
+# cs.send(translation.text.encode('utf-8'))  # encodezi la loc
+#
+# cs.close()
